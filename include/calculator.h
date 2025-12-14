342446d9e95c55709d11d889ed91dae391e6ab11
WalidMaadi <167113119+WalidMaadi@users.noreply.github.com>
2025-12-14 05:18:28 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -4,7 +4,7 @@
 double add(double a, double b);
 double sub(double a, double b);
 double mul(double a, double b);
-double div(double a, double b);
+double division(double a, double b);
 double car(double a);   // fonction carré
 
 #endif
